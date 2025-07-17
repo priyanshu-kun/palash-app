56c77d1d3da80576e5c31a9d3415f46e4c939068
priyanshu-kun <priyanshushrama709@gmail.com>
2025-07-17 09:46:16 +0530

    fix booking  bug

--- apps/server/src/controllers/memberships/memberships.controller.ts
+++ apps/server/src/controllers/memberships/memberships.controller.ts
@@ -689,7 +689,7 @@ export default class MembershipController {
                 const receiptId = `receipt_${uuidv4().replace(/-/g, '')}`;
 
                 const order = await this.razorpay.orders.create({
-                    amount: Number(membershipPlan.cost) * 100,
+                    amount: Number(50000) * 100,
                     currency: "INR",
                     receipt: receiptId,
                     notes: {
